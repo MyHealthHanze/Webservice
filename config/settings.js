3bd3d9cfd40750d0e45d9694baa907a7f87b8347
Bart van 't Ende <bartvtende@gmail.com>
2015-09-16 23:10:27 +0200

    Changed the environment variable for PORT

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     apiUrl: process.env.apiUrl || '/api/v1/',
-    appPort: process.env.appPort || 1337,
+    appPort: process.env.PORT || 1337,
     baseUrl: process.env.baseUrl || 'http://localhost:1337/api/v1/',
     secret: process.env.secret || 'Svj5HeJNAW2E3o2nAuOgEaUx',
     tokenExpiresInMinutes: process.env.tokenExpiresInMinutes || 180
