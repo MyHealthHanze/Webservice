ee18be7e35067ec174c343bfa26297170308ae36
openid12 <openid12@users.noreply.github.com>
2015-09-24 14:42:47 +0200

    Update config.js
    restored to old password

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     "development": {
         "username": process.env.sqlUsername || "root",
-        "password": process.env.sqlPassword ||  "usbw",
+        "password": process.env.sqlPassword ||  null,
         "database": process.env.sqlDatabase || "myhealth",
         "host": process.env.sqlHost || "127.0.0.1",
         "dialect": "mysql"
