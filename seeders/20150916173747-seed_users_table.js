f9008e7d4166f3bdd70fb48197d57f2c1035d2c0
Bart van 't Ende <bartvtende@gmail.com>
2015-09-24 16:57:56 +0200

    Better seed class, should not bug anymore

--- seeders/20150916173747-seed_users_table.js
+++ seeders/20150916173747-seed_users_table.js
@@ -4,11 +4,7 @@ var moment = require('moment');
 
 module.exports = {
     up: function (queryInterface, Sequelize) {
-        createUsersObject('John Bakker', 'johnbakker@gmail.com', 'test', false, queryInterface, null);
-        createUsersObject('Pieter-Jan van Bolgeren', 'pieterjan@gmail.com', 'test', true, queryInterface, moment().utc().add(1, 'years').format("YYYY-MM-DD HH:mm:ss"));
-        createUsersObject('Hermiena Driessen', 'hermienadriessen@gmail.com', 'test', false, queryInterface, null);
-        createUsersObject('Gert Pluim', 'gertpluim@gmail.com', 'test', false, queryInterface, null);
-        createUsersObject('Melissa Teunissen', 'melissateunissen@gmail.com', 'test', false, queryInterface, null);
+        createUsersObjects(queryInterface, 'test');
     },
 
     down: function (queryInterface, Sequelize) {
@@ -16,22 +12,31 @@ module.exports = {
     }
 };
 
-function createUsersObject(name, email, password, changedPassword, queryInterface, disabledOn) {
+function createUsersObjects(queryInterface, password) {
     var bcrypt = require('bcryptjs');
 
     bcrypt.genSalt(10, function (err, salt) {
         bcrypt.hash(password, salt, function (err, hash) {
-            queryInterface.bulkInsert('Users', [{
-                name: name,
-                email: email,
-                password: hash,
-                address: 'Zernikeplein 11',
-                city: 'Groningen',
-                doctorId: 1,
-                changedPassword: changedPassword,
-                amountOfFailedLoginAttempts: 0,
-                disabledOn: disabledOn
-            }], {});
+            createUsersObject('John Bakker', 'johnbakker@gmail.com', hash, false, queryInterface, null);
+            createUsersObject('Pieter-Jan van Bolgeren', 'pieterjan@gmail.com', hash, true, queryInterface, moment().utc().add(1, 'years').format("YYYY-MM-DD HH:mm:ss"));
+            createUsersObject('Hermiena Driessen', 'hermienadriessen@gmail.com', hash, false, queryInterface, null);
+            createUsersObject('Gert Pluim', 'gertpluim@gmail.com', hash, false, queryInterface, null);
+            createUsersObject('Melissa Teunissen', 'melissateunissen@gmail.com', hash, false, queryInterface, null);
         });
     });
-}
+};
+
+
+function createUsersObject(name, email, password, changedPassword, queryInterface, disabledOn) {
+    queryInterface.bulkInsert('Users', [{
+        name: name,
+        email: email,
+        password: password,
+        address: 'Zernikeplein 11',
+        city: 'Groningen',
+        doctorId: 1,
+        changedPassword: changedPassword,
+        amountOfFailedLoginAttempts: 0,
+        disabledOn: disabledOn
+    }], {});
+};
